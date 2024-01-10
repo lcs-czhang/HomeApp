2eaa78ddc958b57eade352edffe212a57742c4ee
Youzhou Zhang <czhang@lcs.on.ca>
2024-01-09 19:40:28 -0500

    Initial Commit

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Yuzhou Zhang on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
